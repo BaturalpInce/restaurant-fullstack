04f541e4290fc0a2787724f9d8c2ce20a5a176f8
BaturalpInce <inceb20@itu.edu.tr>
2023-01-19 20:27:26 +0300

    Fetch and display all items

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,42 +1,64 @@
 import React, { useState, useEffect } from 'react';
-import { collection, getDoc, doc } from "firebase/firestore";
-
-import db from "../firebase.config";
+import { collection, getDocs, doc } from "firebase/firestore";
+import { getDownloadURL, ref } from "firebase/storage";
+import { default as db } from "../firebase.config";
 
 export const MainContainer = () => {
   const [desserts, setDesserts] = useState([]);
-
+  const [mainDishes, setMainDishes] = useState([]);
+  const [drinks, setDrinks] = useState([]);
+  const bucket = "fullstackrestaurant-32f85.appspot.com"
+  const referenceLink = "gs://" + bucket + "images/";
   useEffect(() => {
-    const fetchDesserts = async () => {
-      const docRef = doc(db.db, "menu", "Desserts");
-      const docSnap = await getDoc(docRef);
-      
-      if (docSnap.exists()) {
-        console.log("Document data:", docSnap.data());
-      } else {
-        // doc.data() will be undefined in this case
-        console.log("No such document!");
-      }
-      setDesserts(docSnap.data());
+    const fetchMenuItems = async () => {
+      const snapshot = await getDocs(collection(db.db, "menu"));
+      snapshot.forEach((doc) => {
+        if (doc.id === "Desserts"){
+          setDesserts(doc.data());
+        }
+        else if (doc.id === "Drinks"){
+          setDrinks(doc.data());
+        }
+        else if (doc.id === "Main Dishes"){
+          setMainDishes(doc.data());
+        }
+        console.log(doc.id, " => ", doc.data());
+      });
     }
-    fetchDesserts()
+    fetchMenuItems()
     .catch(console.error);
   }, []);
-
 
   return (
     <div>
       <div><h1>MENU</h1></div>
-      <div><h1>Desserts</h1>
+      <h1>Desserts</h1>
+      <div className="grid md:grid-cols-3 sm:grid-cols-2">
         {Object.keys(desserts).map((dessert) => {
-          console.log(dessert);
-          console.log(desserts[dessert]);
+          const refLinkUpdate = referenceLink + dessert.toLowerCase() + ".jpg";
+          const itemURL = "Images/"+dessert.toLowerCase()+".jpg";
+          const storage = db.storage;
+          const sourceURL = getDownloadURL(ref(storage, itemURL));
+          console.log(sourceURL);
           return ( 
-          <div id={dessert}>
+          <div id={dessert} className="bg-slate-200 mb-4 mt-4 mr-4">
             <h1>{dessert}</h1>
             <h1>{desserts[dessert][0]}</h1>
             <h1>{desserts[dessert][1]}</h1>
             <h1>{desserts[dessert][2]}</h1>
+          </div>
+          );
+        })}
+      </div>
+      <h1>Main Dishes</h1>
+      <div className=" grid md:grid-cols-3 sm:grid-cols-2">
+        {Object.keys(mainDishes).map((mainDish) => {
+          return ( 
+          <div id={mainDish} className="bg-slate-200 mb-4 mt-4 mr-4">
+            <h1>{mainDish}</h1>
+            <h1>{mainDishes[mainDish][0]}</h1>
+            <h1>{mainDishes[mainDish][1]}</h1>
+            <h1>{mainDishes[mainDish][2]}</h1>
           </div>
           );
         })}
