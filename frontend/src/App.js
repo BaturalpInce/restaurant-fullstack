0914001471314247f707893e9406d47911140ac4
BaturalpInce <inceb20@itu.edu.tr>
2023-01-22 19:25:09 +0300

    Updated routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Header } from './components';
-import { MainContainer } from './components';
+import { MenuContainer } from './components';
 import { Routes, Route } from 'react-router-dom'; 
 const App = () => {
   return (
     <div className="w-screen h-auto flex flex-col">
       <Header />
-      <main className="mt-24 p-8 w-full">
+      <main className="mt-20 w-full">
         <Routes>
-          <Route path="/*" element={<MainContainer />} />
+          <Route path="/" element={<MenuContainer />} />
+          <Route path="/home" element={<MenuContainer />} />
+          <Route path="/menu" element={<MenuContainer />} />
         </Routes>
       </main>
     </div>
